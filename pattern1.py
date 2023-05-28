d0b60a1820be12ee6fcf159770bb334fd9b954d1
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2023-05-29 00:03:49 +0530

    Solid Rectangle or Square

---
+++ pattern1.py
@@ -0,0 +1,11 @@
+# Solid Rectangle or Square
+
+# for square make rows = columns
+
+rows = 5
+columns = 8
+
+for i in range(rows):
+    for j in range(columns):
+        print("*", end=" ")
+    print()
